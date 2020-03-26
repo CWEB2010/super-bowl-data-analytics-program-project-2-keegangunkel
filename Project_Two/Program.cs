c98a3f813fcddfe567877ead6ef1de7cf8a5ce40
Gunkel <gunkeec@dunwoody.edu>
2020-03-26 16:35:10 -0500

    Made the file stream and started the process of printing out the winning teams with the specifications.

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -9,8 +9,13 @@ namespace Project_Two
             /**Your application should allow the end user to pass end a file path for output 
             * or guide them through generating the file.
             **/
+            // USE GET FULL PATH FOR THE READ FILE FOR EXTRA CREDIT POINT 
+            Console.WriteLine("Please enter the file you want to read in.");
+            string file = Console.ReadLine();
+            //string PATH = Path.GetFullPath(file);
+            string PATH = @$"C:\Users\gunkeec\Desktop\Project2-2\Project2\{file}";
 
-           
+
         }
     }
 }
